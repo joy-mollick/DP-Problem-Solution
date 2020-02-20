43bd589fe8e9464e4a8455af0106ff5b17d30f6b
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2020-02-19 17:21:07 -0800

    Create Leetcode-698. Partition to K Equal Sum Subsets(Bitmask&dp).cpp

---
+++ Leetcode-698. Partition to K Equal Sum Subsets(Bitmask&dp).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int n = nums.size(), sum = 0;
+        int total_state=1<<n;
+        int dp[total_state];
+        memset(dp,-1,sizeof(dp));
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) sum += nums[i];
+        if (sum % k) return false;
+        int tar = sum/k;
+        
+        for (int state = 0; state < (total_state); state++) {
+            if (dp[state] == -1) continue;  // if current state is illegal, simply ignore it
+            for (int i = 0; i < n; i++) {
+                if (!(state&(1<<i)) && dp[state]+nums[i] <= tar) {  // if nums[i] is unchosen && choose nums[i] would not cross the target
+                    int new_state=state|(1<<i);//ith number will be choosen 
+                    dp[new_state] = (dp[state]+nums[i]) % tar;
+                }
+            }
+        }
+        return dp[total_state-1] == 0;
+    }
+};
